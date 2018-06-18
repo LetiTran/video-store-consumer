63a25d2e81437adbd5048c94a7a5d4ce5b2db144
Kate Pond <kate@thepondsedge.com>
2018-06-18 16:55:50 -0700

    starts components folder

---
+++ src/components/Movie.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Movie extends Component {
+
+
+  render () {
+    return (
+
+    )
+  }
+}
+
+export default Movie;
+
+Movie.propTypes = {
+}
