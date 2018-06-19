ca7f4b9b740adf13c7bde6985d979e6fb63ea5f2
Kate Pond <kate@thepondsedge.com>
2018-06-19 13:02:12 -0700

    adds router and header to app js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CustomerCollection from './components/CustomerCollection.js'
+import LibraryCollection from './components/LibraryCollection.js'
 import SearchResults from './components/SearchResults.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        </header>
 
-        <CustomerCollection />
-      </div>
+
+  render() {
+    const Library = () => (
+      <LibraryCollection />
+    )
+
+    const Customers = () => (
+      <CustomerCollection />
+    )
+
+    return (
+      <Router>
+        <div className="App">
+          <header>
+            <h1>THE Video Store</h1>
+            <nav>
+              <ul>
+                <li>
+                  <p>Search Placeholder</p>
+                </li>
+                <li>
+                  <Link to="/library">Library</Link>
+                </li>
+                <li>
+                  <Link to="/customers">Customers</Link>
+                </li>
+              </ul>
+
+              <Route path="/library" component={Library} />
+              <Route path="/customers" component={Customers} />
+            </nav>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
